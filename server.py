cc04e6f1f3089dfd760710ff0e1db39ff9592dd6
externref <shiva02939@gmail.com>
2025-02-21 22:06:44 +0530

    update server

--- server.py
+++ server.py
@@ -1,21 +1,27 @@
 import io
 
 import fastapi
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile, Form
 from fastapi.responses import Response
 from PIL import Image, ImageDraw, ImageFont
 
-app = fastapi.FastAPI()
-
+app = FastAPI()
 
 @app.post("/apply")
-async def apply(file: UploadFile):
+async def apply(file: UploadFile, addWatermark: bool = Form(False)):
     try:
         image_bytes = await file.read()
         image = Image.open(io.BytesIO(image_bytes))
-        image_draw = ImageDraw.Draw(image)
-        fontface = ImageFont.load_default()
-        image_draw.text((10, 10), "anishot_", font=fontface)
+        
+        if addWatermark:
+            image_draw = ImageDraw.Draw(image)
+            fontface = ImageFont.load_default()
+            text = "anishot_"
+            text_width, text_height = image_draw.textsize(text, font=fontface)
+            
+            position = (image.width - text_width - 10, image.height - text_height - 10)
+            image_draw.text(position, text, font=fontface)
+        
         img_byte_array = io.BytesIO()
         image.save(img_byte_array, format="PNG")
         img_byte_array.seek(0)
